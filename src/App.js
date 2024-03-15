54ce500ebb9f9f8995fc76146b2b149fbbb31fad
haja mydeen <120375547+haja2611@users.noreply.github.com>
2024-03-15 10:25:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./Components/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
+      <Navbar />
       <Home />
       <Footer />
     </div>
